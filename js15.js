60424a65eb83f55515e8da14c835494f99942383
Aliaksei Redzko <aleha.redko@gmail.com>
2022-12-21 02:35:19 +0200

    fers commit at abstrackt udemy lesons

---
+++ js15.js
@@ -0,0 +1,34 @@
+"user strict";
+
+console.log("arr" + "- object");
+console.log( 4 + +"5");
+
+let incr = 10;
+
+let decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8') 
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
+
+const isCheckedOne = true,
+      isCloseTwo= true;
+
+console.log(isCheckedOne || isCloseTwo);
+
+console.log(2 + 2 * 2 != 8);
+
+console.log(2 + 2 * 2 != '6');
+
+console.log(2 + 2 * 2 !== '6')
