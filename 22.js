6d0fb5988b11667f1284ca9e65a18f65fa2ab130
Aliaksei Redzko <aleha.redko@gmail.com>
2023-04-04 13:34:42 +0300

    add lelons on operator

---
+++ 22.js
@@ -0,0 +1,31 @@
+'use strict';
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+console.log(4 + '5');
+console.log(4 + +' - object');
+
+
+let incr = 10; 
+    decr = 10; 
+ 
+incr ++;/* инкремент увеличивает значение на 1 (постфиксная запись - после переменной)*/
+decr--;/* декремент ументшвет значение на 1 (постфиксная запись - после переменной)*/
+
+++incr; /*постфиксная запись*/
+--dec;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8'); /* строгое сравнение*/
+console.log(2*4 == '8'); /* не строгое сравнение*/
+
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+
